45dc70abdab363a850af1a92884aafbb1569b4bc
mohamilin <amilinmoh99@gmail.com>
2020-11-28 23:42:36 +0700

    setup myprofile

---
+++ src/components/Header.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+import "./StylesComponent.css";
+import logo from "../logo.svg";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    height: 37,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    float: "right",
+    marginRight: 30,
+    marginTop: 5
+  },
+  appBar: {
+    height: 60,
+    backgroundColor: '#004d40'
+  },
+}));
+
+export default function Header() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.appBar}>
+        <div className="sub-app-header">
+          <div className="cont-toolbar">
+            <div>
+              <img
+                className="App-logo"
+                src={logo}
+              />
+             
+              <Typography variant="h6" className={classes.title}>
+               amilin's website
+              </Typography>
+            </div>
+          </div>
+        </div>
+      </AppBar>
+    </div>
+  );
+}
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Paper, Grid } from "@material-ui/core";
+import HomeIcon from "@material-ui/icons/Home";
+import CodeIcon from "@material-ui/icons/Code";
+import PhoneAndroidIcon from "@material-ui/icons/PhoneAndroid";
+import BookIcon from "@material-ui/icons/Book";
+
+import "./StylePages.css";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: "#01579b",
+    backgroundColor: "#f9fbe7",
+  },
+}));
+
+function LandingPage() {
+  const classes = useStyles();
+
+  return (
+    <>
+      <div style={{ textAlign: "center" }}>
+        <Paper className={classes.paper}>
+          <Grid container spacing={3}>
+            <Grid item xs={6} sm={3}>
+              <HomeIcon fontSize="large" />
+              <p className="menu">Home</p>
+            </Grid>
+            <Grid item xs={6} sm={3}>
+              <CodeIcon fontSize="large" />
+              <p className="menu">Portofolio</p>
+            </Grid>
+            <Grid item xs={6} sm={3}>
+              <PhoneAndroidIcon fontSize="large" />
+              <p className="menu">Contact</p>
+            </Grid>
+            <Grid item xs={6} sm={3}>
+              <BookIcon fontSize="large" />
+              <p className="menu">Blog</p>
+            </Grid>
+          </Grid>
+        </Paper>
+      </div>
+      <div>
+      <p>Halaman Landing</p>
+
+      </div>
+    </>
+  );
+}
+
+export default LandingPage;
