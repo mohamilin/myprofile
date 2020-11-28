355becf449a580ae43ae2b49c4ed7a79d46a66b3
mohamilin <amilinmoh99@gmail.com>
2020-11-28 23:49:20 +0700

    setup myprofile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
+// import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+// import Button from "@material-ui/core/Button";
+// import IconButton from "@material-ui/core/IconButton";
+// import MenuIcon from "@material-ui/icons/Menu";
 
 import "./StylesComponent.css";
 import logo from "../logo.svg";
@@ -42,6 +42,7 @@ export default function Header() {
               <img
                 className="App-logo"
                 src={logo}
+                alt="logo"
               />
              
               <Typography variant="h6" className={classes.title}>
