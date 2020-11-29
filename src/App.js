1cedd676ab0ebf1f9ffe2b0edc2e97a084e8125d
mohamilin <amilinmoh99@gmail.com>
2020-11-29 14:31:17 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 // import logo from "./logo.svg";
 import "./App.css";
+import { Paper, Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 import Header from "./components/Header";
 import LandingPage from "./pages/LandingPage";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+
   return (
     <>
-       <Header/>
+      <Header/>
       <div className="app-test">
         <div className="sub-app">
           <div className="content">
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 // import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
+// import Typography from "@material-ui/core/Typography";
 // import Button from "@material-ui/core/Button";
 // import IconButton from "@material-ui/core/IconButton";
 // import MenuIcon from "@material-ui/icons/Menu";
@@ -21,13 +21,12 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
     float: "right",
-    marginRight: '5%',
-    marginTop: 5
+    marginRight: "5%",
+    marginTop: 5,
   },
   appBar: {
     height: 60,
-    width: "100%",
-    backgroundColor: '#004d40'
+    backgroundColor: "#004d40",
   },
 }));
 
@@ -36,23 +35,15 @@ export default function Header() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static" className={classes.appBar}>
-        <div className="sub-app-header">
+      <div className="sub-app-header">
+        <AppBar position="static" className={classes.appBar}>
           <div className="cont-toolbar">
             <div>
-              <img
-                className="App-logo"
-                src={logo}
-                alt="logo"
-              />
-             
-              <Typography variant="h6" className={classes.title}>
-               amilin's website
-              </Typography>
+              <img className="App-logo" src={logo} alt="logo" />
             </div>
           </div>
-        </div>
-      </AppBar>
+        </AppBar>
+      </div>
     </div>
   );
 }
