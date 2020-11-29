95311e23c17b5509c5ba87340128fb54f8fa9ce3
mohamilin <amilinmoh99@gmail.com>
2020-11-29 14:38:58 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 // import logo from "./logo.svg";
 import "./App.css";
 // import { Paper, Grid } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+// import { makeStyles } from "@material-ui/core/styles";
 
 import Header from "./components/Header";
 import LandingPage from "./pages/LandingPage";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  },
-}));
+
 
 function App() {
-  const classes = useStyles();
 
   return (
     <>
